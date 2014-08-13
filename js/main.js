f119efd0c56213d7777083eaf5e752fc9f57a718
Conner Fritz <me@connerfritz.com>
2014-08-13 11:14:21 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+define(["./thousandwords"],
+  function (ThousandWords) {
+    var thousandwords = new ThousandWords("div#cropper", {src: "img/PIA18461.jpg", width: 1920, height: 1080, canvasWidth: 500, canvasHeight: 500});
+  }
+)
