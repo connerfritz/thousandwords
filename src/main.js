89eaa56feee821fe1f6657712006b5031a6c57cc
Conner Fritz <me@connerfritz.com>
2014-08-13 11:35:41 -0400

    Add AMD module

--- src/main.js
+++
@@ -1,8 +0,0 @@
-define(["./thousandwords"],
-  function (ThousandWords) {
-    var thousandwords = new ThousandWords("div#wow", {src: "PIA18461.jpg", width: 1920, height: 1080});
-    document.onclick = function() {
-      console.log(thousandwords.getImage());
-    }
-  }
-)
